b6605b7f61bc614c073ef367e1404a44f1a2e652
samee <sameer754811@gmail.com>
2025-05-22 16:22:07 +0530

    work done by 22/05/2025

---
+++ lib/constants.ts
@@ -0,0 +1,8 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'Moltres'
+
+export const APP_SLOGAN =
+  process.env.NEXT_PUBLIC_APP_SLOGAN || 'Where Gamers Trade & Upgrade.'
+
+export const APP_DESCRIPTION =
+  process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
+  'A platform where you can find friends for gaming and buy or sell your PS titles'
---
+++ lib/data.ts
@@ -0,0 +1,34 @@
+const data = {
+  headerMenus: [
+    {
+      name: "sell your PS titles",
+      href: '/search?tag=todays-deal',
+    },
+    {
+      name: 'Find Players',
+      href: '/search?tag=new-arrival',
+    },
+    {
+      name: 'Featured Products',
+      href: '/search?tag=featured',
+    },
+    {
+      name: 'Best Sellers',
+      href: '/search?tag=best-seller',
+    },
+    {
+      name: 'Customer Service',
+      href: '/page/customer-service',
+    },
+    {
+      name: 'About Us',
+      href: '/page/about-us',
+    },
+    {
+      name: 'Help',
+      href: '/page/help',
+    },
+  ],
+}
+
+export default data
