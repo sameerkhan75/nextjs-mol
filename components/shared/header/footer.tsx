ecc2030f23b15537f9d36da6c44ee5499ec10103
samee <sameer754811@gmail.com>
2025-07-05 17:10:42 +0530

    update

--- components/shared/header/footer.tsx
+++ components/shared/header/footer.tsx
@@ -2,7 +2,6 @@
 
 import { ChevronUp } from 'lucide-react'
 import { Button } from '../../ui/button'
-import Link from 'next/link'
 import { APP_NAME } from '@/lib/constants'
 
 export default function Footer() {
