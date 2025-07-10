c95e14017c31545a8ffd13e8907f4fedc8898f2b
samee <sameer754811@gmail.com>
2025-07-11 03:00:00 +0530

    update

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,4 @@
 import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
-import clientPromise from "@/lib/db/mongodb";
 import { authOptions } from "@/app/api/auth/[...nextauth]/authOptions";
 
 const handler = NextAuth(authOptions);
