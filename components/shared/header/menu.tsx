8d0589c01dd401306a3c26b80d0ec657d807801b
samee <sameer754811@gmail.com>
2025-07-21 23:39:23 +0530

    update

--- components/shared/header/menu.tsx
+++ components/shared/header/menu.tsx
@@ -10,18 +10,21 @@ export default function Menu() {
     <div className='flex justify-end'>
       <nav className='flex gap-3 w-full'>
         {loading ? null : session ? (
-          <button onClick={() => signOut()} className='header-button flex items-center gap-2'>
-            <UserIcon className='h-8 w-8' />
-            <span className='font-bold'>Sign out</span>
-          </button>
+          <>
+            <span className='font-bold flex items-center gap-2'>
+              <UserIcon className='h-8 w-8' />
+              {session.user?.name?.split(' ')[0]}
+            </span>
+            <button onClick={() => signOut()} className='header-button flex items-center gap-2'>
+              <span className='font-bold'>Sign out</span>
+            </button>
+          </>
         ) : (
           <button onClick={() => signIn('google')} className='header-button flex items-center gap-2'>
             <UserIcon className='h-8 w-8' />
             <span className='font-bold'>Sign in with Google</span>
           </button>
         )}
-
-
       </nav>
     </div>
   )
