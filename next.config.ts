aba9a886137dc7d38176b6fbd9a18d31f5126a69
samee <sameer754811@gmail.com>
2025-07-29 11:54:49 +0530

    update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ['nextjs-mol.s3.eu-north-1.amazonaws.com'],
+  },
 };
 
 export default nextConfig;
