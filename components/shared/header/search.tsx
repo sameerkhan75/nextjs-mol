d777c424583292cc6741ca581f000cd0800f7c6f
samee <sameer754811@gmail.com>
2025-08-02 10:08:29 +0530

    fixed search orientation

--- components/shared/header/search.tsx
+++ components/shared/header/search.tsx
@@ -14,10 +14,10 @@ export default async function Search() {
   const categories = await getAllCategories()
 
   return (
-    <form action='/search' method='GET' className='flex items-stretch h-9'>
+    <form action='/search' method='GET' className='flex items-stretch h-10'>
       <Select name='category'>
-        <SelectTrigger className='w-auto h-full dark:border-gray-200 bg-gray-100 text-black border-r rounded-r-none rounded-l-md'>
-          <SelectValue placeholder='All' />
+        <SelectTrigger className='w-auto h-full dark:border-gray-200 bg-gray-100 text-black border-r rounded-l-md rounded-r-none'>
+          <SelectValue placeholder='All Categories' />
         </SelectTrigger>
         <SelectContent position='popper'>
           <SelectItem value='all'>All</SelectItem>
@@ -29,16 +29,16 @@ export default async function Search() {
         </SelectContent>
       </Select>
       <Input
-        className='flex-1 rounded-none dark:border-gray-200 bg-gray-100 text-black text-base h-full'
+        className='flex-1 rounded-none dark:border-gray-200 bg-gray-100 text-black text-base h-full border-x-0'
         placeholder={`Search ${APP_NAME}`}
         name='q'
         type='search'
       />
       <button
         type='submit'
-        className='bg-primary text-primary-foreground text-black rounded-s-none rounded-e-md h-full px-3 py-2'
+        className='bg-yellow-400 hover:bg-yellow-500 text-black rounded-l-none rounded-r-md h-full px-4 flex items-center justify-center transition-colors'
       >
-        <SearchIcon className='w-6 h-6' />
+        <SearchIcon className='w-5 h-5' />
       </button>
     </form>
   )
