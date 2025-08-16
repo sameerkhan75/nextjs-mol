ab7c1cc663146bc94c2b54f66c5d1591d353190e
samee <sameer754811@gmail.com>
2025-08-16 23:53:44 +0530

    update

--- app/(root)/page/help/page.tsx
+++ app/(root)/page/help/page.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
-
+//add the authorization feature correctly
 export default function HelpPage() {
   return (
     <div style={{
